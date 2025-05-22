39f8eb6b12005e0126902312fb47e77b9f52d2b0
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2025-05-22 13:53:10 -0300

    setup inicial

---
+++ src/main/java/br/dev/bertolo/apidevsuperior/ApiDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package br.dev.bertolo.apidevsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevsuperiorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevsuperiorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/bertolo/apidevsuperior/ApiDevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.bertolo.apidevsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevsuperiorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
