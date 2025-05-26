35ee0386181b6c1386b7ecdd224b344b1f9585f6
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2025-05-25 21:32:05 -0300

    fix: Retirando crase da renomeção do campo year para ser compatível com o postgresql

--- src/main/java/br/dev/bertolo/apidevsuperior/projections/GameMinProjection.java
+++ src/main/java/br/dev/bertolo/apidevsuperior/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package br.dev.bertolo.apidevsuperior.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
